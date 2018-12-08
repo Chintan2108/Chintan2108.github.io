54f2a3adc4126c598b72419aecf5e4e709837259
Chintan2108 <chintanmaniyar@gmail.com>
2018-12-08 20:05:40 +0530

    portfolio

---
+++ scripts/main.js
@@ -0,0 +1,62 @@
+// Add your javascript here
+// Don't forget to add it into respective layouts where this js file is needed
+
+$(document).ready(function() {
+  AOS.init( {
+    // uncomment below for on-scroll animations to played only once
+    // once: true  
+  }); // initialize animate on scroll library
+});
+
+// Smooth scroll for links with hashes
+$('a.smooth-scroll')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
+
+function updateHeading(id) {
+  if(id.localeCompare('rs')==0) {
+    document.getElementById('heading').innerHTML = "My Remote Sensing Projects"
+  }
+  else if (id.localeCompare('ml')==0) {
+    document.getElementById('heading').innerHTML = "My Machine Learning Projects"
+  }
+  else if (id.localeCompare('app')==0) {
+    document.getElementById('heading').innerHTML = "My Standalone Applications and Websites"
+  }
+  else if (id.localeCompare('pub')==0) {
+    document.getElementById('heading').innerHTML = "My Research and Publications"
+  }
+  else if (id.localeCompare('crt')==0) {
+    document.getElementById('heading').innerHTML = "My Certifications"
+  }
+  
+}
--- server-scripts/transcripts-request-onsubmit.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	$student_id = $_GET['student_id'];
-	session_start();
-	$_SESSION['student_id'] = $student_id;
-	
-	echo $student_id;
-?>
